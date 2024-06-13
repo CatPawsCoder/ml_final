6e91797f004b1fc604d1f255d7f9478703461b38
Meme <dariabespal@gmail.com>
2024-06-13 20:27:17 +0300

    Добавлен скрипт для создания датасета Titanic

---
+++ src/create_dataset.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+from catboost.datasets import titanic
+
+def main():
+    # Создаем папку, если она не существует
+    output_dir = '../data/raw'  # Путь относительно src/
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Загружаем датасет
+    train, _ = titanic()
+
+    # Выбираем необходимые колонки
+    dataset = train[['Pclass', 'Sex', 'Age']]
+
+    # Путь для сохранения файла CSV
+    output_file = os.path.join(output_dir, 'titanic_dataset.csv')
+
+    # Сохраняем датасет в файл CSV
+    dataset.to_csv(output_file, index=False)
+
+    print(f'Dataset сохранен в {output_file}')
+
+if __name__ == "__main__":
+    main()
